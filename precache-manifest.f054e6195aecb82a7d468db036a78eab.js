3402c9b9465426adf6cfb60656b3b6d10476e2a9
dziobakwszafie <dziobakwszafie@gmail.com>
2020-01-28 12:34:57 +0100

    Updates

---
+++ precache-manifest.f054e6195aecb82a7d468db036a78eab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5993e764ae7aa0167da5caf8832821a7",
+    "url": "/ski-service/index.html"
+  },
+  {
+    "revision": "001705b8d941ff63e171",
+    "url": "/ski-service/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ee83f0c61575a6d539db",
+    "url": "/ski-service/static/css/main.d3bc950d.chunk.css"
+  },
+  {
+    "revision": "001705b8d941ff63e171",
+    "url": "/ski-service/static/js/2.e494f145.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ski-service/static/js/2.e494f145.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee83f0c61575a6d539db",
+    "url": "/ski-service/static/js/main.f44e9162.chunk.js"
+  },
+  {
+    "revision": "7ca9e32266bd0b458590",
+    "url": "/ski-service/static/js/runtime-main.a379b28d.js"
+  },
+  {
+    "revision": "8c3ebee5d901be5744d5645bfab73ffe",
+    "url": "/ski-service/static/media/sms.8c3ebee5.png"
+  },
+  {
+    "revision": "a0ebcc96bbf3257b6458c005bb1141bb",
+    "url": "/ski-service/static/media/whatsapp.a0ebcc96.png"
+  }
+]);
