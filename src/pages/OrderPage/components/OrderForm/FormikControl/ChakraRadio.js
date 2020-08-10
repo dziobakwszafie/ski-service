51521d334958dfc1587228690787748a6b5c4ecf
dziobakwszafie <dziobakwszafie@gmail.com>
2020-08-10 12:18:47 +0200

    feature: add chakra radio button

--- src/pages/OrderPage/components/OrderForm/FormikControl/ChakraRadio.js
+++ src/pages/OrderPage/components/OrderForm/FormikControl/ChakraRadio.js
@@ -1,35 +1,44 @@
 import React from "react";
-import { Field, ErrorMessage } from "formik";
-import TextError from "./TextError";
-import { Radio, RadioGroup } from "@chakra-ui/core";
+import { Field } from "formik";
+import {
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from "@chakra-ui/core";
 
-function RadioButtons(props) {
+function ChakraRadio(props) {
   const { label, name, options, ...rest } = props;
   return (
-    <div className="form-control">
-      <label>{label}</label>
-      <Field name={name}>
-        {({ field }) => {
-          return options.map((option) => {
-            return (
-              <React.Fragment key={option.key}>
-                <input
-                  type="radio"
-                  id={option.value}
-                  {...field}
-                  {...rest}
-                  value={option.value}
-                  checked={field.value === option.value}
-                />
-                <label htmlFor={option.value}>{option.key}</label>
-              </React.Fragment>
-            );
-          });
-        }}
-      </Field>
-      <ErrorMessage component={TextError} name={name} />
-    </div>
+    <Field name={name}>
+      {({ field, form }) => (
+        <FormControl isInvalid={form.errors[name] && form.touched[name]}>
+          <FormLabel htmlFor={name}>{label}</FormLabel>
+          <RadioGroup isInline>
+            {options.map((option) => {
+              return (
+                <React.Fragment key={option.key}>
+                  <Radio
+                    htmlFor={option.value}
+                    id={option.value}
+                    {...field}
+                    {...rest}
+                    value={option.value}
+                    checked={field.value === option.value}
+                    variantColor="red"
+                  >
+                    {option.key}
+                  </Radio>
+                </React.Fragment>
+              );
+            })}
+          </RadioGroup>
+          <FormErrorMessage>{form.errors[name]}</FormErrorMessage>
+        </FormControl>
+      )}
+    </Field>
   );
 }
 
-export default RadioButtons;
+export default ChakraRadio;
