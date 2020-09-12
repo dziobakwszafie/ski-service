d64baef7558f7af075176c511ea7d1dfe1bdf695
dziobakwszafie <dziobakwszafie@gmail.com>
2020-09-12 22:51:55 +0200

    feature: add user credentials to store after login

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -15,6 +15,19 @@ export const login = (loginData) => (dispatch) => {
       const FBIdToken = `Bearer ${res.data.token}`;
       localStorage.setItem("FBIdToken", FBIdToken);
       axios.defaults.headers.common["Authorization"] = FBIdToken;
+      dispatch(getUserData());
       dispatch({ type: SET_LOADING });
     });
+};
+
+export const getUserData = () => (dispatch) => {
+  axios
+    .get("/user")
+    .then((res) => {
+      dispatch({
+        type: SET_USER,
+        payload: res.data,
+      });
+    })
+    .catch((err) => console.log(err));
 };
